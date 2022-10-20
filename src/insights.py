3bb2b7fb9f1b0b07669157362002d16af0a158a6
SheilaNS <shei.nsantos@gmail.com>
2022-10-20 15:43:11 -0300

    feat(requisito 06): finalizado filter_by_job_types implementado e passando no teste

--- src/insights.py
+++ src/insights.py
@@ -12,21 +12,11 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    all_job_types = []
+    for job in jobs:
+        if job['job_type'] == job_type:
+            all_job_types.append(job)
+    return all_job_types
 
 
 def get_unique_industries(path):
