76b03181a572884a6a6700fc6af46ad3227a1aa9
SheilaNS <shei.nsantos@gmail.com>
2022-10-20 15:22:29 -0300

    feat(requirito 04): finalizado get_max_salary implementada e passando no teste

--- src/insights.py
+++ src/insights.py
@@ -58,21 +58,12 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    jobs_list = read(path)
+    all_salary = []
+    for job in jobs_list:
+        if job['max_salary'].isdigit():
+            all_salary.append(int(job['max_salary']))
+    return max(all_salary)
 
 
 def get_min_salary(path):
