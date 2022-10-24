affc64379f362948c9fc5f1a46f5a73642ed3450
SheilaNS <shei.nsantos@gmail.com>
2022-10-23 22:48:50 -0300

    teste(requisito 10): iniciado test_counter iniciado, mas passando com falso positivo

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    'Se a função conta corretamente as palavras Python e JavaScript'
+    assert count_ocurrences("src/jobs.csv", "Python") == 1639
+    assert count_ocurrences("src/jobs.csv", "JavaScript") == 122
